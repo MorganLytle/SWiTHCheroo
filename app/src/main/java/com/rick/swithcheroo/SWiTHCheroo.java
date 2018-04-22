cb430e667d46657e12e888d167624281124c7fb8
Diego <diegot1@uci.edu>
2018-04-21 20:31:25 -0700

    menu icons added

--- app/src/main/java/com/rick/swithcheroo/SWiTHCheroo.java
+++ app/src/main/java/com/rick/swithcheroo/SWiTHCheroo.java
@@ -1,15 +1,23 @@
 package com.rick.swithcheroo;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class SWiTHCheroo extends AppCompatActivity {
+public class SWiTHCheroo extends Activity  {
 
     private TextView mTextMessage;
+    // Capture our button from layout
+
+    // Register the onClick listener with the implementation above
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,7 +28,7 @@ public class SWiTHCheroo extends AppCompatActivity {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
+                case R.id.navigation_dashboard :
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
@@ -30,16 +38,44 @@ public class SWiTHCheroo extends AppCompatActivity {
             return false;
         }
 
+
+
+
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swi_thcheroo);
-
+        ImageButton button = (ImageButton)findViewById(R.id.channel_1);
+        ImageButton button1 = (ImageButton)findViewById(R.id.channel_2);
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+//        GameView gameView = new GameView(this);
+        button.setOnClickListener( new View.OnClickListener()
+        {
+            @Override
+            public void onClick (View v){
+                System.out.print("\nchannel 1\n");
+                next_page(v);
+            }
+        });
+        button1.setOnClickListener( new View.OnClickListener()
+        {
+            @Override
+            public void onClick (View v){
+                System.out.print("\nchannel 2\n");
+                next_page(v);
+            }
+        });
+
+
     }
+    public void next_page(View v) {
+        Intent intent = new Intent(this, SWiTHCheroo.class);
+        startActivity(intent);
+    }
+
 
 }
